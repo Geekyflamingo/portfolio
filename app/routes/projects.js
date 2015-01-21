1ed156a41ad046d8982e14cb45104419de3afbef
Becca <rebecca.vasil@gmail.com>
2015-01-21 16:26:10 -0700

    main page looking good

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return [
+      {id: 1, title: 'foo', body: 'lorem ipsum'},
+      {id: 2, title: 'bar', body: 'lorem ipsum'},
+      {id: 3, title: 'baz', body: 'lorem ipsum'}
+    ];
+  }
 });
