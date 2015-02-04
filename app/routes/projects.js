f0e030d93c3df4dacddfa2e16b998a1db3e3bdb8
Becca <rebecca.vasil@gmail.com>
2015-02-04 14:48:25 -0700

    changed some wording

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -10,7 +10,7 @@ export default Ember.Route.extend({
         description: "It is a Hunter/Jumper course designer for the ambitious equestrian. It will be able to eliminate the pen and paper way of designing courses. It comes with the functionality to customize each jump by choosing the colors and type. Then to position the jump just drag and drop with the mouse. It can even be rotated into different orientations. ",
         url: "https://calm-springs-6334.herokuapp.com/",
         github: "https://github.com/Geekyflamingo/Personal_Project",
-        skills: ["Ruby on Rails", "Javascript", "FabricJS", "JSON", "User Authorization", "Bootstrap", "Associations"],
+        skills: ["Ruby on Rails", "Javascript", "FabricJS", "HTML5 Canvas","JSON", "User Authorization", "Bootstrap" ],
         image: "/Screen Shot_Course1.png",
         image_2: "/Screen Shot_Course2.png",
       },
