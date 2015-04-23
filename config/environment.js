93cd5a7607d18c000cbcff100a6fc15b1bc8d9e2
Becca <rebecca.vasil@gmail.com>
2015-04-23 13:59:10 -0600

    Css fix for fire fox still working on making it look better but MVP is done

--- config/environment.js
+++ config/environment.js
@@ -12,7 +12,15 @@ module.exports = function(environment) {
         // e.g. 'with-controller': true
       }
     },
-
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self' http://fonts.googleapis.com/css?family=Raleway:400,,800,900 https://fonts.gstatic.com",
+      'connect-src': "'self'",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    },
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
